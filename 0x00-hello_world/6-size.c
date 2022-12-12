524a3a3ac38b8dbfc3191e7fb954af0465b36290
temesgenweyna <temesgenweyna7@gmail.com>
2022-12-12 09:12:38 -0800

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
  * Return: Always 0 (Success)
  */
 
-
 int main(void)
 {
 
-	printf("Size of s char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
@@ -20,4 +19,3 @@ int main(void)
 	return (0);
 
 }
-
