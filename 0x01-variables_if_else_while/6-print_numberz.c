468b637476ac85d1701e0452371ac96634291fc2
temesgenweyna <temesgenweyna7@gmail.com>
2022-12-11 23:10:09 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - returns single numbers from 0 - 9
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
